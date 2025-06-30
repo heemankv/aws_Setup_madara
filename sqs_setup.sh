82e008ab8f292250ff568a5722dd6f18384cdc97
Heemank Verma <heemankv@gmail.com>
2025-06-30 19:08:42 +0530

    Update sqs_setup.sh

--- sqs_setup.sh
+++ sqs_setup.sh
@@ -20,7 +20,7 @@ EOF
 echo "redrive-policy.json created."
 
 # Create WorkerTrigger queue
-WORKER_TRIGGER_QUEUE_NAME=$(get_queue_name "WorkerTrigger")
+WORKER_TRIGGER_QUEUE_NAME=$(get_queue_name "worker_trigger")
 aws sqs create-queue --queue-name "${WORKER_TRIGGER_QUEUE_NAME}" \
   --attributes VisibilityTimeout=300 --region "${AWS_REGION}" --profile "$AWS_PROFILE_NAME"
 
@@ -28,7 +28,7 @@ export WORKER_TRIGGER_QUEUE_URL=$(aws sqs get-queue-url --queue-name "${WORKER_T
 export WORKER_TRIGGER_QUEUE_ARN=$(aws sqs get-queue-attributes --queue-url "${WORKER_TRIGGER_QUEUE_URL}" --attribute-names QueueArn --query 'Attributes.QueueArn' --output text --region "${AWS_REGION}" --profile "$AWS_PROFILE_NAME")
 
 # Create other queues with DLQ
-QUEUE_TYPES=("SnosJobProcessing" "SnosJobVerification" "ProvingJobProcessing" "ProvingJobVerification" "ProofRegistrationJobProcessing" "ProofRegistrationJobVerification" "DataSubmissionJobProcessing" "DataSubmissionJobVerification" "UpdateStateJobProcessing" "UpdateStateJobVerification")
+QUEUE_TYPES=("snos_job_processing" "snos_job_verification" "proving_job_processing" "proving_job_verification" "proof_registration_job_processing" "proof_registration_job_verification" "data_submission_job_processing" "data_submission_job_verification" "update_state_job_processing" "update_state_job_verification")
 VISIBILITY_TIMEOUTS=(300 300 300 300 300 300 300 300 900 300)
 
 for i in "${!QUEUE_TYPES[@]}"; do
